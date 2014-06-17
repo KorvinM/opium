9a5bd33287084c6c0825b7245dfdad10bc0bac46
Korv <km@korvin.org>
2014-06-17 09:45:44 +0100

    redesign tests layout

--- index.php
+++ index.php
@@ -12,40 +12,43 @@ include('includes/header.php');
 		<header>
 			<h1>Tests</h1>
         </header>
-		<section class="col-1-2">
-			<h2>Config Tests</h2>
-			<h3>Output values from the config global in init.php</h3>
-			<p>
-			<?php echo 'echo cookie expiry time: <span class="php-output">' . Config::get('remember/cookie_expiry') . '</span>';
-			echo '<br>var_dump same: <span class="php-output">';
-			var_dump(Config::get('remember/cookie_expiry')); 
-			echo '</span>'; ?>
-			<p>
-			<?php echo 'echo session name: <span class="php-output">' . Config::get('session/session_name') . '</span>';
-			echo '<br>var_dump same: <span class="php-output">';
-			var_dump(Config::get('session/session_name')); 
-			echo '</span>'; ?>
-			<p>
-			<?php echo 'echo random false value foo/bar: <span class="php-output">' . Config::get('foo/bar') . '</span>'; ?>
-			<br>To var_dump 'foo/bar' would reveal the contents of the <code>$GLOBALS['config']</code> variable.<br>
-			The codebase could be improved to test against this case!
-			<p>var_dump an empty value only returns false: 
-			<span class="php-output"> <?php var_dump( Config::get('') ); ?></span>
-								 
+		<section class="">
+			<div class="col-1-1">
+				<h2>Config Tests</h2>
+				<h3>Output values from the config global in init.php</h3>
+				<p>
+				<?php echo 'Cookie expiry time. echo: <span class="php-output">' . Config::get('remember/cookie_expiry') . '</span>';
+				echo ' var_dump: <span class="php-output">';
+				var_dump(Config::get('remember/cookie_expiry')); 
+				echo '</span>'; ?>
+				<p>
+				<?php echo 'session name. echo: <span class="php-output">' . Config::get('session/session_name') . '</span>';
+				echo ' var_dump: <span class="php-output">';
+				var_dump(Config::get('session/session_name')); 
+				echo '</span>'; ?>
+				<p>
+				<?php echo 'echo random false value foo/bar: <span class="php-output">' . Config::get('foo/bar') . '</span>'; ?>
+				<br>To var_dump 'foo/bar' would reveal the contents of the <code>$GLOBALS['config']</code> variable.<br>
+				The codebase could be improved to test against this case!
+				<p>var_dump an empty value only returns false: 
+				<span class="php-output"> <?php var_dump( Config::get('') ); ?></span>
+			</div>						 
                            
 		</section>
-		<section class="col-1-2">
-			<h2>Database tests</h2>
-			<h3>Test Connection to DB class</h3>
-			<span class="php-output">
-				<?php if(class_exists('DB')){//http://stackoverflow.com/questions/22064419/trying-to-figure-out-how-to-check-if-an-class-has-been-instantiated
-					DB::getInstance();
-                    echo 'Connected';
-                 } else{
-                    echo 'Not connected';
-                 } ?>
-            </span>
-
+		<section class="">
+			<div class="col-1-1">
+				<h2>Database tests</h2>
+				<h3>Test Connection to DB class</h3>
+				<span class="php-output">
+					<?php if(class_exists('DB')){//http://stackoverflow.com/questions/22064419/trying-to-figure-out-how-to-check-if-an-class-has-been-instantiated
+						DB::getInstance();
+						echo 'Connected';
+					 } else{
+						echo 'Not connected';
+					 } ?>
+				</span>
+			</div>
+			<div class="col-1-1">
 
 			<h3>Basic method tests</h3>
 			<p>First test: prepare and execute query<br>
@@ -73,9 +76,8 @@ include('includes/header.php');
 	}</pre>		</code>
 							
 				<?php 
-					echo '<br><span class="php-output">';
+					echo '<span class="php-output">';
 					$user = DB::getInstance()->query("SELECT username FROM users WHERE username =?", array('billy'));
-					echo '<br>';
 					if($user->count()){
 						echo 'Found user';
 									
@@ -94,7 +96,6 @@ include('includes/header.php');
 				<?php
 					echo '<span class="php-output">';
 					$user = DB::getInstance()->get('users',array('username','=','billy'));
-					echo '<br>';
 					if($user->count()){
 						echo 'Found user';
 					} else{
@@ -102,6 +103,9 @@ include('includes/header.php');
 					}
 					echo '</span>';
 				?>
+			</div>
+			
+			<div class="col-1-2">	
 			<h3>Read Data</h3> 
 			<p>get all users with standard sql query:<br>
 				<code><pre>$user = DB::getInstance()->query("SELECT * FROM users");
@@ -135,7 +139,6 @@ include('includes/header.php');
 				<?php
 					echo '<span class="php-output">';
 					$user = DB::getInstance()->get('users',array('username','=','billy'));
-					echo '<br>';
 					if($user->count()){
 						echo $user->first()->username;	
 					} else{
@@ -143,6 +146,9 @@ include('includes/header.php');
 					}
 					echo '</span>';
 				?>
+				
+			</div>
+			<div class="col-1-2">	
 			<h3>Insert and Update Data</h3>
 			<p>The code to insert data would be something like:<br> 
 				<code><pre>$user = DB::getInstance()->insert('users', array(
@@ -155,7 +161,7 @@ include('includes/header.php');
 				<code><pre>$user = DB::getInstance()->update('users',3, array(
 	'password' => 'updatedpassword',
 	));</pre>	</code>	
-		
+	</div>	
 		</section>
 		<section class="col-1-1">
 			<p><a href="register.php">Proceed to Register</a>
