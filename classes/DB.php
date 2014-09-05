6bb581b960ccfff15488de4e0e0e11cea49f8252
Korvin <KorvinM@users.noreply.github.com>
2014-09-05 01:47:27 +0100

    Update DB.php
    correct indentation

--- classes/DB.php
+++ classes/DB.php
@@ -8,33 +8,33 @@ class DB{
 	private static $_instance = null;//_ is notation to denote a private or protected property of the class
 	
 	private $_pdo,
-			$_query,
-			$_error = false,
-			$_results,//stores results set
-			$_count = 0;
+		$_query,
+		$_error = false,
+		$_results,//stores results set
+		$_count = 0;
 			
 	private function __construct(){
 		
-			try {
-				$this->_pdo = new PDO('mysql:host=' . Config::get('mysql/host') . ';dbname=' . Config::get('mysql/db'),Config::get('mysql/username'),Config::get('mysql/password'));
-				//'Connected';
-			} catch(PDOException $e){
-				die($e->getMessage());
-			  }
+		try {
+			$this->_pdo = new PDO('mysql:host=' . Config::get('mysql/host') . ';dbname=' . Config::get('mysql/db'),Config::get('mysql/username'),Config::get('mysql/password'));
+			//'Connected';
+		} catch(PDOException $e){
+			die($e->getMessage());
+		}
 			  
 	}//end constructor
 	
 	public static function getInstance(){//available outside the class
-			if(!isset(self::$_instance)){//if database is not instantiated
-				self::$_instance = new DB();//instantiate; will run contructor above
-			}
-			return self::$_instance;//if $_instance has already been set, just return existing instance
+		if(!isset(self::$_instance)){//if database is not instantiated
+			self::$_instance = new DB();//instantiate; will run contructor above
+		}
+		return self::$_instance;//if $_instance has already been set, just return existing instance
 	}
 	
 	public function query($sql, $params = array()){
 		$this->_error = false;//reset error to false, in case there's one from a previous query
 		if($this->_query=$this->_pdo->prepare($sql)){//prepare & test, bind params & execute
-	    //'Query prepared successfully';
+	    		//'Query prepared successfully';
 			$pos = 1;
 			if (count($params)){//check parameters exist and bind
 				foreach($params as $param){
@@ -116,15 +116,13 @@ class DB{
 			$set .= "{$name} = ?";
 			if( $x <  count($fields) ){
 					$set .= ', ';
-				}
-				$x++;
-				
-		}
-		;
+			}
+			$x++;
+		};
 		$sql = "UPDATE {$table} SET {$set} WHERE id = {$id}" ;
 		if(!$this->query($sql,$fields)->error()){
-				return true;
-			}
+			return true;
+		}
 		return false;
 		echo $sql;
 	}
